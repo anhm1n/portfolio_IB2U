54fbbc67ba3bc6339c15676c79a02b5af7ffa242
anhm1n <anhm1n@uw.edu>
2018-10-02 21:45:14 -0700

    Remove early graphs and add permutations

--- swim_stats.R
+++ swim_stats.R
@@ -33,6 +33,8 @@ y4 <- filter(swim_groups, Level == "Y4")
 y3 <- filter(swim_groups, Level == "Y3")
 p1 <- filter(swim_groups, Level == "P1")
 
+swim_main <- filter(swim_groups, Level == "Y4" | Level == "Y3" | Level == "P1")
+
 # create plots to show distribution based on level and sex
 ggplot(data = swim_groups) +
   geom_histogram(mapping = aes(x = Age, fill = Level), binwidth = 1) +
@@ -46,6 +48,11 @@ ggplot(data = swim_groups) +
   scale_y_continuous(breaks = seq(0, 18, by=2)) +
   labs(title = "Age Distribution of Students by Sex")
 
+ggplot(data = swim_main) +
+  geom_boxplot(mapping = aes(x = Level, y = Age, fill = Pass)) +
+  coord_flip() +
+  labs(title = "Age Distribution by Passing of Main Levels")
+
 # create tibbles to have main levels averages
 y4_avg <- group_by(y4, Pass) %>% 
   summarize(average = mean(Age), maximum = max(Age), minimum = min(Age), st_dev = sd(Age))
@@ -54,46 +61,42 @@ y3_avg <- group_by(y3, Pass) %>%
 p1_avg <- group_by(p1, Pass) %>% 
   summarize(average = mean(Age), maximum = max(Age), minimum = min(Age), st_dev = sd(Age))
 
-
 # Bootstrap, keep students age and Sex, randomize pass fail
 passOrFail <- c("Y", "N")
 sims <- 10000
 
+y4_passes <- y4[, "Pass"]
+y3_passes <- y3[, "Pass"]
+p1_passes <- p1[, "Pass"]
+
 # Bootstrap to create a vector of randomly generated population means
-bootstrap_age <- function(df, diff_age = F) {
-  listMeans <- sample(passOrFail, size = nrow(df), replace = T) 
-  simDF <- mutate(df, Pass = listMeans)
-  if (diff_age == F) {
-    mean(simDF[simDF$Pass == "Y", 'Age'], na.rm = T)  
-  } else {
-    mean(simDF[simDF$Pass == "Y" & simDF$Sex == "M", "Age"], na.rm = T) - 
-      mean(simDF[simDF$Pass == "Y" & simDF$Sex == "F", "Age"], na.rm = T)    
-  }
+bootstrap_age <- function(df) {
+  passing <- sample(passOrFail, size = nrow(df), replace = T) 
+  simDF <- mutate(df, Pass = passing)
+  mean(simDF[simDF$Pass == "Y", 'Age'], na.rm = T)  
+}
+
+permute_age <- function(df, list_pass) {
+  passing <- sample(list_pass, size = nrow(df), replace = F) 
+  simDF <- mutate(df, Pass = passing)
+  mean(simDF[simDF$Pass == "Y", "Age"], na.rm = T)
 }
 
 # Plot and draw confidence interval lines for bootstrap
-mean_age_plot <- function(means, avg, class, diff_age = F) {
+mean_age_plot <- function(means, avg, class) {
   CI_vals <- quantile(means, prob=c(0.025,0.975), na.rm = T)
-  if (diff_age == F) {
-    hist(means, main = paste("Average Passing Ages", class), xlab = "Age")
-    abline(v = c(CI_vals, avg[2, 2]), lwd = 3, col = c("blue", "blue", "red"), lty = c(1, 1, 2))
-    legend("topright", legend = c(paste0("CI 95% [", round(CI_vals[1], 3), ", ", round(CI_vals[2], 3), "]"), 
-                                  paste("Observed Mean:", round(avg[2, 2], 3))), 
-          col = c("blue", "red"), lty = c(1, 2, 2), lwd = 2, cex = 0.7)
-  } else {
-    hist(means, main = paste("Average Passing Age Differences (M - F)", class), xlab = "Age")
-    abline(v = c(CI_vals, avg[2, 2] - avg[1, 2]), lwd = 3, col = c("blue", "blue", "red"), lty = c(1, 1, 2))
-    legend("topright", legend = c(paste0("CI 95% [", round(CI_vals[1], 3), ", ", round(CI_vals[2], 3), "]"), 
-                                  paste("Observed Mean Difference:", round(avg[2,2] - avg[1, 2], 3))), 
-           col = c("blue", "red"), lty = c(1, 2, 2), lwd = 2, cex = 0.7)  
-  }
+  hist(means, main = paste("Average Passing Ages", class), xlab = "Age")
+  abline(v = c(CI_vals, avg[2, 2]), lwd = 3, col = c("blue", "blue", "red"), lty = c(1, 1, 2))
+  legend("topright", legend = c(paste0("CI 95% [", round(CI_vals[1], 3), ", ", round(CI_vals[2], 3), "]"), 
+                                paste("Observed Mean:", round(avg[2, 2], 3))), 
+        col = c("blue", "red"), lty = c(1, 2, 2), lwd = 2, cex = 0.7)
 }
 
 mean_age_plot(replicate(n = sims, bootstrap_age(y4)), y4_avg, "Y4")
 mean_age_plot(replicate(n = sims, bootstrap_age(y3)), y3_avg, "Y3")
 mean_age_plot(replicate(n = sims, bootstrap_age(p1)), p1_avg, "P1")
 
-mean_age_plot(replicate(n = sims, bootstrap_age(y4, diff_age = T)), y4_avg, "Y4", diff_age = T)
-# mean_age_plot(replicate(n = sims, bootstrap_age(y3, diff_age = T)), y3_avg, "Y3", diff_age = T)
-mean_age_plot(replicate(n = sims, bootstrap_age(p1, diff_age = T)), p1_avg, "P1", diff_age = T)
+mean_age_plot(replicate(n = sims, permute_age(y4, y4_passes)), y4_avg, "Y4")
+mean_age_plot(replicate(n = sims, permute_age(y3, y3_passes)), y3_avg, "Y3")
+mean_age_plot(replicate(n = sims, permute_age(p1, p1_passes)), p1_avg, "P1")
 
