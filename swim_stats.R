a18023bc8b46e7545d3770a9bbb6035380098987
anhm1n <anhm1n@uw.edu>
2018-10-02 22:46:19 -0700

    Delete unnecessary plots and calculations

--- swim_stats.R
+++ swim_stats.R
@@ -4,31 +4,7 @@ library(tidyr)
 
 swim_groups <- read.csv("swimresults.csv", stringsAsFactors = F)
 
-# Create a table on the conditional that the student passed
-swim_pass <- filter(swim_groups, Pass == "Y")
-
-swim_levels <- group_by(swim_groups, Level) %>% 
-  summarize(average = mean(Age), count = n())
-
-swim_pass_level_male <- swim_pass %>% 
-  filter(Sex == "M") %>% 
-  group_by(Level) %>% 
-  summarize(count = n())
-
-swim_pass_level_fem <- swim_pass %>% 
-  filter(Sex == "F") %>% 
-  group_by(Level) %>% 
-  summarize(count = n())
-
-swim_pass_array <- as.data.frame(full_join(by = "Level", swim_pass_level_male, swim_pass_level_fem))
-swim_pass_array[is.na(swim_pass_array)] <- 0
-swim_pass_array <- mutate(swim_pass_array, Percentage_Male = count.x / nrow(swim_pass),
-                          Percentage_Fem = count.y / nrow(swim_pass))
-colnames(swim_pass_array)[c(2, 3)] <- c("Count_Male", "Count_Fem")
-
-write.csv(swim_pass_array, file = "SwimPassArray.csv")
-
-#create data frames of 3 largest levels
+# Create data frames of 3 largest levels
 y4 <- filter(swim_groups, Level == "Y4")
 y3 <- filter(swim_groups, Level == "Y3")
 p1 <- filter(swim_groups, Level == "P1")
@@ -40,13 +16,7 @@ ggplot(data = swim_groups) +
   geom_histogram(mapping = aes(x = Age, fill = Level), binwidth = 1) +
   scale_x_continuous(breaks = 1:16) +
   scale_y_continuous(breaks = seq(0, 18, by=2)) +
-  labs(title = "Age Distribution of Students by Level")
-
-ggplot(data = swim_groups) +
-  geom_histogram(mapping = aes(x = Age, fill = Sex), binwidth = 1) +
-  scale_x_continuous(breaks = 1:16) +
-  scale_y_continuous(breaks = seq(0, 18, by=2)) +
-  labs(title = "Age Distribution of Students by Sex")
+  labs(title = "Students' Ages by Level", x = "Age (1-Year Range)")
 
 ggplot(data = swim_main) +
   geom_boxplot(mapping = aes(x = Level, y = Age, fill = Pass)) +
@@ -65,24 +35,26 @@ p1_avg <- group_by(p1, Pass) %>%
 passOrFail <- c("Y", "N")
 sims <- 10000
 
+# Select the true pass and fails for permutations
 y4_passes <- y4[, "Pass"]
 y3_passes <- y3[, "Pass"]
 p1_passes <- p1[, "Pass"]
 
-# Bootstrap to create a vector of randomly generated population means
+# Bootstrap to return randomly generated passing student age
 bootstrap_age <- function(df) {
   passing <- sample(passOrFail, size = nrow(df), replace = T) 
   simDF <- mutate(df, Pass = passing)
   mean(simDF[simDF$Pass == "Y", 'Age'], na.rm = T)  
 }
 
+# Permute to return randomly generated passing student age
 permute_age <- function(df, list_pass) {
   passing <- sample(list_pass, size = nrow(df), replace = F) 
   simDF <- mutate(df, Pass = passing)
   mean(simDF[simDF$Pass == "Y", "Age"], na.rm = T)
 }
 
-# Plot and draw confidence interval lines for bootstrap
+# Plot and draw confidence interval lines for bootstrap/permutation
 mean_age_plot <- function(means, avg, class) {
   CI_vals <- quantile(means, prob=c(0.025,0.975), na.rm = T)
   hist(means, main = paste("Average Passing Ages", class), xlab = "Age")
@@ -92,10 +64,12 @@ mean_age_plot <- function(means, avg, class) {
         col = c("blue", "red"), lty = c(1, 2, 2), lwd = 2, cex = 0.7)
 }
 
+# Bootstrap and plot 'sims' times
 mean_age_plot(replicate(n = sims, bootstrap_age(y4)), y4_avg, "Y4")
 mean_age_plot(replicate(n = sims, bootstrap_age(y3)), y3_avg, "Y3")
 mean_age_plot(replicate(n = sims, bootstrap_age(p1)), p1_avg, "P1")
 
+# Permute and plot 'sims' times
 mean_age_plot(replicate(n = sims, permute_age(y4, y4_passes)), y4_avg, "Y4")
 mean_age_plot(replicate(n = sims, permute_age(y3, y3_passes)), y3_avg, "Y3")
 mean_age_plot(replicate(n = sims, permute_age(p1, p1_passes)), p1_avg, "P1")
